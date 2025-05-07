dfacb4dac537322d3e085f76e5992bc7468c2b27
Jan Gerardi <jan_gerardi@outlook.com>
2025-05-07 20:19:04 +0200

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function init(){
 }
 
 async function getData(){
-    let url = `https://pokeapi.co/api/v2/pokemon?limit=60&offset=0`
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=30&offset=0`
     let response = await fetch(url);
     let currentRequest = await response.json();
     renderRequest(currentRequest)
@@ -27,4 +27,22 @@ function toUppercase(text){
 
 function addHashtag(id){
     return '#' + id;
+}
+
+async function getMoreData(){
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=60&offset=0`
+    let response = await fetch(url);
+    let currentRequest = await response.json();
+    renderMore(currentRequest)
+}
+
+async function renderMore(currentRequest){
+    let currentObject = currentRequest.results;
+    let contentContainer = document.getElementById("pokedex");
+    for (let i = 30; i < 60; i++) {
+        const currentPokemon = currentObject[i];
+        let pokeDetailsResponse = await fetch(currentPokemon.url);
+        let currentPokemonDetails = await pokeDetailsResponse.json();
+        contentContainer.innerHTML += pokedexTemplate(currentPokemonDetails);
+    }
 }
--- template.js
+++ template.js
@@ -2,15 +2,15 @@ function pokedexTemplate(pokemon){
     const type1 = pokemon.types[0]?.type.name || "";
     const type2 = pokemon.types[1]?.type.name || "";
     return /*html*/`
-            <div class="pokedexCard">
+            <div class="pokedexCard ${type1}">
                 <div class="pokedexCardTop">
                     <h3>${toUppercase(pokemon.name)}</h3>
                     <p class="pokemonId">${addHashtag(pokemon.id)}</p>
                 </div>
                 <div class="pokedexCardBottom">
                     <div>
-                        <p>${toUppercase(type1)}</p>
-                        ${type2 ? `<p>${toUppercase(type2)}</p>` : ""}
+                        <p class="show_${type1}">${toUppercase(type1)}</p>
+                        ${type2 ? `<p class="show_${type2}">${toUppercase(type2)}</p>` : ""}
                     </div>
                     <img src="${pokemon.sprites.front_default}">
                 </div>
